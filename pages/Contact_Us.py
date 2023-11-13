5b0ba7daa3cbeac4335c8ecfcba58e466a625c82
otakayui <yueeee145@gmail.com>
2023-11-14 06:16:47 +0900

    send emails using the form.

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,11 +1,17 @@
 import streamlit as st
+from send_emmail import send_email
 
 st.header("Contact Me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
-    massage = st.text_area("Your message")
+    raw_message = st.text_area("Your message")
+    message = f"""\
+Subject: New email from {user_email}
+From: {user_email}
+{raw_message}
+    """
     button = st.form_submit_button("Submit")
     if button:
-       
-        print("I was pressed!")
+       send_email(message)
+       st.info("Your email was sent successfully.")
--- send_emmail.py
+++ send_emmail.py
@@ -1,19 +1,16 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "otky623@gmail.com"
-password = "xrjvwhjjhaaklzop"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "otky623@gmail.com"
+    password = "xrjvwhjjhaaklzop"
+    receiver = "otky623@gmail.com"
+    context = ssl.create_default_context()
 
-receiver = "otky623@gmail.com"
-context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-message = """\
-Subject: Hi! 
-How are you?
-bye!
-"""
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+
